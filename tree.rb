d25008a2c02a2115c8d798d8656177ca49d415f4
ssimkani <seenasimkani@gmail.com>
2024-06-20 09:41:42 -0400

    Add method for building balanced binary search tree using recursion

--- tree.rb
+++ tree.rb
@@ -1,11 +1,26 @@
 # frozen_string_literal: true
 
+require_relative 'node'
+
 # Class that represents the binary search tree
 class Tree
-  attr_accessor :root
+  attr_accessor :root, :arr
 
   def initialize(arr)
     @arr = arr
     @root = nil
+  end
+
+  def build_tree(array)
+    return if array.empty?
+
+    array = array.sort.uniq
+    mid_index = ((array.length - 1) / 2).to_i
+    root = Node.new(array[mid_index])
+
+    root.left = build_tree(array[0...mid_index])
+    root.right = build_tree(array[(mid_index + 1)..])
+
+    root
   end
 end
