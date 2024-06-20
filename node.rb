fc5b34784638d146b501e21bb1f6bac7efdf11f5
ssimkani <seenasimkani@gmail.com>
2024-06-20 09:11:56 -0400

    Add Node class with initialized value, left, and right nodes

---
+++ node.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Class for the nodes of the binary search tree
+class Node
+  include Comparable
+
+  attr_accessor :value, :left, :right
+
+  def initialize(value, left = nil, right = nil)
+    @value = value
+    @left = left
+    @right = right
+  end
+end
